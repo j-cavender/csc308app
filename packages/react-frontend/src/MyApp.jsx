10de2938a816d517fa710305febcd018f4aa424d
Josh Cavender <jcavende@calpoly.edu>
2025-01-16 10:09:31 -0800

    Connected POST functionality

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -14,11 +14,26 @@ function MyApp() {
     }
 
     function updateList(person) {
-        setCharacters([...characters, person]);
+        postUser(person)
+            .then(() => setCharacters([...characters, person]))
+            .catch((error) => {
+                console.log(error);
+            })
     }
 
     function fetchUsers() {
         const promise = fetch("http://localhost:8000/users");
+        return promise;
+    }
+
+    function postUser(person) {
+        const promise = fetch("http://localhost:8000/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(person),
+        });
         return promise;
     }
 
@@ -28,7 +43,7 @@ function MyApp() {
             .then((json) => setCharacters(json["users_list"]))
             .catch((error) => { console.log(error); });
     }, [] );
-    
+
     return (
         <div className="container">
             <Table 
