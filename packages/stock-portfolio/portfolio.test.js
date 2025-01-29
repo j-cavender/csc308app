a8067944f3330c9e5f7d6b4fc29266b5f553f61d
Josh Cavender <jcavende@calpoly.edu>
2025-01-28 21:25:56 -0800

    Refactor into beforeach

--- packages/stock-portfolio/portfolio.test.js
+++ packages/stock-portfolio/portfolio.test.js
@@ -1,11 +1,9 @@
 /*
 I believe I was able to follow the test-first approach going over the red-green-refactor cycle
 for the most part.  I was able to follow the goal of building a test then working to build minimal
-code to meet those goals.  I will admit I was a bit lax on the refactor step of the process as within my
-testing there is a bit of duplication that definitely could have been reduced.  Outside of that there wasn't
-too much refactoring required to meet the tests in the actual code bar a step or two.  Obviously plenty could
-be done in the long term scale of things as more testing and building is added as it is very simple in its
-current form.
+code to meet those goals.  I think I could have probably refactored a bit more adamantly.  Obviously
+plenty could be done in the long term scale of things as more testing and building is added as it is very
+simple in its current form.
 
 All in all I feel that the focus of the exercise was to showcase how breaking a project down into a
 simple step-by-step process in order to keep your thoughts and code within a narrow focus. It feels like an
@@ -14,32 +12,33 @@ extremely powerful approach that I will hopefully be able to hone a bit more thr
 
 const myPortfolio = require('./portfolio.js');
 
+let portfolio;
+
+beforeEach(() => {
+    portfolio = myPortfolio.createPortfolio();
+});
+
 test('Create Stock Portfolio', () => {
-    const portfolio = myPortfolio.createPortfolio();
     expect(portfolio).toBeDefined();
 });
 
 test('Empty Portfolio Check', () => {
-    const portfolio = myPortfolio.createPortfolio();
     const emptyPortfolio = myPortfolio.shareStatus(portfolio);
     expect(emptyPortfolio).toBe('No shares owned');
 });
 
 test('Make a purchase', () => {
-    const portfolio = myPortfolio.createPortfolio();
     myPortfolio.purchaseStock(portfolio, "GOOGL", 34);
     expect(portfolio.find(stock => "GOOGL" in stock)["GOOGL"].shares).toBe(34);
 });
 
 test('Make a sale', () => {
-    const portfolio = myPortfolio.createPortfolio();
     myPortfolio.purchaseStock(portfolio, "AMZN", 15);
     myPortfolio.sellStock(portfolio, "AMZN", 14);
     expect(portfolio.find(stock => "AMZN" in stock)["AMZN"].shares).toBe(1);
 });
 
 test('Unique ticker symbols', () => {
-    const portfolio = myPortfolio.createPortfolio();
     myPortfolio.purchaseStock(portfolio, "AMZN", 15);
     myPortfolio.purchaseStock(portfolio, "GOOGL", 14);
     const stockCount = myPortfolio.countStock(portfolio);
@@ -47,23 +46,20 @@ test('Unique ticker symbols', () => {
 });
 
 test('Remove unowned stocks', () => {
-    const portfolio = myPortfolio.createPortfolio();
     myPortfolio.purchaseStock(portfolio, "AMZN", 0);
     const cleanPortfolio = myPortfolio.cleanupStocks(portfolio);
     expect(cleanPortfolio).toEqual([]);
 });
 
 test('Check shares for a symbol', () => {
-    const portfolio = myPortfolio.createPortfolio();
     myPortfolio.purchaseStock(portfolio, "AMZN", 13);
-    currentShares = myPortfolio.getShares(portfolio, "AMZN");
-    zeroShares = myPortfolio.getShares(portfolio, "GOOGL");
+    const currentShares = myPortfolio.getShares(portfolio, "AMZN");
+    const zeroShares = myPortfolio.getShares(portfolio, "GOOGL");
     expect(currentShares).toBe(13);
     expect(zeroShares).toBe(0);
 });
 
 test('Do not oversell shares', () => {
-    const portfolio = myPortfolio.createPortfolio();
     myPortfolio.purchaseStock(portfolio, "AMZN", 13);
     expect(() => myPortfolio.sellStock(portfolio, "AMZN", 44)).toThrow(Error);
 })
